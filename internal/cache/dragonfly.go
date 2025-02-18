2b03cf0be4ded63a4e3714229f3f3d38d41bcec3
Dmitriy Bukatsel <dmitriy.bukatsel@gmail.com>
2025-02-18 23:37:45 +0300

    prevent rawClient from accessing in different file

--- internal/cache/dragonfly.go
+++ internal/cache/dragonfly.go
@@ -14,17 +14,17 @@ const (
 )
 
 type dragonfly struct {
-	RawClient *redis.Client
+	rawClient *redis.Client
 }
 
 var Dragonfly = dragonfly{
-	RawClient: redis.NewClient(&redis.Options{
+	rawClient: redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	}),
 }
 
 func (dg *dragonfly) SetUniqueKey(ctx context.Context, key string, value any, exp time.Duration) (bool, error) {
-	isKeyUnique, err := dg.RawClient.SetNX(ctx, key, value, exp).Result()
+	isKeyUnique, err := dg.rawClient.SetNX(ctx, key, value, exp).Result()
 	if err != nil {
 		return false, fmt.Errorf("error while setting to dragonfly db: %v", err)
 	}
@@ -34,7 +34,7 @@ func (dg *dragonfly) SetUniqueKey(ctx context.Context, key string, value any, ex
 
 func (dg *dragonfly) AcquireLock(ctx context.Context) error {
 	for {
-		isKeySet, err := dg.RawClient.SetNX(ctx, REDIS_LOCK_KEY, "", time.Second*10).Result()
+		isKeySet, err := dg.rawClient.SetNX(ctx, REDIS_LOCK_KEY, "", time.Second*10).Result()
 		if err != nil {
 			return fmt.Errorf("failed on setting lock key: %v", err)
 		}
@@ -60,7 +60,7 @@ func (dg *dragonfly) AcquireLock(ctx context.Context) error {
 }
 
 func (dg *dragonfly) ReleaseLock(ctx context.Context) error {
-	_, err := dg.RawClient.Del(ctx, REDIS_LOCK_KEY).Result()
+	_, err := dg.rawClient.Del(ctx, REDIS_LOCK_KEY).Result()
 	if err != nil {
 		return fmt.Errorf("failed on deleting lock key: %v", err)
 	}
