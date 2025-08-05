87b4f14d4e19ceb38000f3022a31feedb730c022
Dima Bukatsel <dimasgrand16@gmail.com>
2025-08-05 18:34:30 +0300

    updated redis script to return current timestamp

--- internal/generator-storage/redis-script.lua
+++ internal/generator-storage/redis-script.lua
@@ -6,17 +6,27 @@ if not timestamp then
     redis.call("SET", KEYS[2], timestamp)
 end
 
+timestamp = tonumber(timestamp)
+
+local newTimestamp = tonumber(redis.call("TIME")[1])
+if newTimestamp > timestamp then
+    timestamp = newTimestamp
+    multiplier = 1
+
+    redis.call("SET", KEYS[1], 1)
+end
+
 if multiplier > tonumber(ARGV[1]) then
-    local newTimestamp = redis.call("TIME")[1]
-    if newTimestamp == tonumber(timestamp) then
-        newTimestamp = newTimestamp + 1
+    while (newTimestamp == timestamp) do
+        newTimestamp = tonumber(redis.call("TIME")[1])
     end
 
     timestamp = newTimestamp
     multiplier = 1
 
     redis.call("SET", KEYS[1], 1)
-    redis.call("SET", KEYS[2], timestamp)
 end
+
+redis.call("SET", KEYS[2], timestamp)
 
 return {multiplier, timestamp}
